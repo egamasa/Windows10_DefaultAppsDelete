465970f6f1802147fbd6967bc1a3f6791da28282
egamasa <egamasa@orangeliner.net>
2018-06-27 00:03:14 +0900

    first commit

---
+++ DeleteApps.ps1
@@ -0,0 +1,34 @@
+﻿Get-AppxPackage *3d* | Remove-AppxPackage                               # 3D Builder
+Get-AppxPackage king.com.CandyCrushSodaSaga | Remove-AppxPackage        # Candy Crush Soda Saga
+Get-AppxPackage Facebook.Facebook | Remove-AppxPackage                  # Facebook
+Get-AppxPackage *zune* | Remove-AppxPackage                             # Groove ミュージック、映画、テレビ
+Get-AppxPackage KeeperSecurityInc.Keeper | Remove-AppxPackage           # Keeper
+Get-AppxPackage NAVER.LINE* | Remove-AppxPackage                        # LINE
+Get-AppxPackage *solit* | Remove-AppxPackage                            # Microsoft Solitaire Collection
+Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage           # OneNote
+Get-AppxPackage *people* | Remove-AppxPackage                           # People
+Get-AppxPackage flaregamesGmbH.RoyalRevolt2 | Remove-AppxPackage        # RoyalRevolt 2
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage                 # Skypeを始めよう
+Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage             # Sticky Notes
+Get-AppxPackage *Twitter | Remove-AppxPackage                           # Twitter
+Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage        # View 3D
+Get-AppxPackage *xbox* | Remove-AppxPackage                             # Xbox
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage                  # Xbox(AU以降)
+Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage          # Xbox(AU以降)
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage     # Xbox(AU以降)
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage  # Xbox(AU以降)
+Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage                    # アラーム＆クロック
+Get-AppxPackage *camera* | Remove-AppxPackage                           # カメラ
+Get-AppxPackage Microsoft.5220175982889 | Remove-AppxPackage            # テレBing
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage               # はじめに
+Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage       # フィードバックハブ
+Get-AppxPackage *photo* | Remove-AppxPackage                            # フォト
+Get-AppxPackage Microsoft.MSPaint | Remove-AppxPackage                  # ペイント3D
+Get-AppxPackage *soundrec* | Remove-AppxPackage                         # ボイスレコーダー
+Get-AppxPackage A278AB0D.MarchofEmpires | Remove-AppxPackage            # マーチ オブ エンパイア
+Get-AppxPackage *bing* | Remove-AppxPackage                             # マネー、スポーツ、ニュース、天気
+Get-AppxPackage *communi* | Remove-AppxPackage                          # メール、カレンダー
+Get-AppxPackage *messaging* | Remove-AppxPackage                        # メッセージング
+Get-AppxPackage *phone* | Remove-AppxPackage                            # モバイル コンパニオン
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage       # 新しいOffice を始めよう
+Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage               # 有料Wi-Fi & 携帯ネットワーク
